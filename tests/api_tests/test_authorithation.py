95c8fcd09b96fd69b6ab6d4454317532347ced1b
Stanislav <stanislavlaptenok@gmail.com>
2024-06-18 10:35:11 +0300

    little change for auth tests

--- tests/api_tests/test_authorithation.py
+++ tests/api_tests/test_authorithation.py
@@ -13,13 +13,13 @@ class TestAuthorization:
 
     def test_valid_auth(self):
 
-        self.auth.check_authorization(payload=self.data.data_for_login_admin())
-        self.auth.check_status_is_200ok()
+        check = self.auth.check_authorization(payload=self.data.data_for_login_admin())
+        self.auth.check_status_is_200ok(check)
 
     def test_invalid_auth(self):
 
-        self.auth.check_authorization(payload=self.data.invalid_data_for_login())
-        self.auth.check_status_is_401_unauthorized()
+        check = self.auth.check_authorization(payload=self.data.invalid_data_for_login())
+        self.auth.check_status_is_401_Unauthorized(check)
 
 
 
